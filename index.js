a039234c8a18a6c699a3aa21c957698c0607762c
John <jwbarand@gmail.com>
2024-10-09 13:53:37 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer(){
+    var bestCustomer = 'not bob';
+    window.bestCustomer = bestCustomer;
+}
+
+function overwriteBestCustomer(newBest){
+    bestCustomer = newBest;
+}
+
+const leastFavoriteCustomer = 'some customer';
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'another customer';
+}
